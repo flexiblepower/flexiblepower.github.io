8ebda20447a5b8e9761e1a63270eae9fb30ad5e0
Frank Molenaar <frank.molenaar@ogd.nl>
2014-10-29 21:19:57 +0100

    search engine added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -64,13 +64,12 @@ $(document).ready(function(){
 		});
 	}
 
-	setTimeout(function() {
-		console.log("bam");		
-			$("iframe#twitter-widget-0").css({
-				"width": "100%",
-				"height": "200px",
-				"visibility": "visible"
-			});
+	setTimeout(function() {	
+		$("iframe#twitter-widget-0").css({
+			"width": "100%",
+			"height": "200px",
+			"visibility": "visible"
+		});
 	}, 1000);
 	
 
@@ -79,4 +78,11 @@ $(document).ready(function(){
 	$(window).resize(function(){		
 		changeIntroHeight();		
 	});
+
+    $('#search-query').lunrSearch({
+      indexUrl: '/search.json',             // URL of the `search.json` index data for your site
+      results:  '#search-results',          // jQuery selector for the search results container
+      entries:  '.entries',                 // jQuery selector for the element to contain the results list, must be a child of the results element above.
+      template: '#search-results-template'  // jQuery selector for the Mustache.js template
+    });
 });
---
+++ search.md
@@ -0,0 +1,4 @@
+---
+layout: search
+permalink: /search/
+---
